42657a52ae9d2ad63f452f591c116f56bee2ea52
Nathan Rydel <nathan@nathanrydel.dev>
2024-11-04 22:02:08 -0700

    Project Setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,29 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+// import { loadEnv } from 'vite';
+import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
+import robotsTxt from 'astro-robots-txt';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    tailwind(
+      {
+        applyBaseStyles: false,
+      }
+    ),
+    react(),
+    robotsTxt(),
+  ],
+  vite: {
+    resolve: {
+      alias: {
+        '@': new URL('./src', import.meta.url).pathname,
+      }
+    }
+  },
+  output: 'hybrid',
+  adapter: vercel()
+});
